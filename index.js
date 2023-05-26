f654ac9cc3fb8485a2a083a19648d1f113a6fc67
Mahmoud Haj Ali <mahmoudhajali2000@gmail.com>
2023-05-26 16:34:52 +0300

    modify

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const middleware = require('./others/middleware');
 const userRouter = require('./routes/user.route');
 const appRouter = require('./routes/app.route');
 
-const port = 8080;
+const port = process.env.MYSQL_ADDON_PORT ?? 8080;
 const app = express();
 
 // Middle Ware
@@ -14,7 +14,7 @@ app.use(middleware);
 app.use(userRouter);
 app.use(appRouter);
 
-app.listen(port, "185.42.117.108", async () => {
+app.listen(port, "0.0.0.0", async () => {
     await dbConnection.sync();
     console.log(`Example app listening on port ${port}`)
 })
