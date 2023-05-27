8426315387b05cc5e3cf28c2123da96dfb4715f5
Mahmoud Haj Ali <mahmoudhajali2000@gmail.com>
2023-05-27 23:11:45 +0300

    reset password

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.use(userRouter);
 app.use(appRouter);
 
 app.listen(port,"0.0.0.0", async () => {
-    await dbConnection.sync();
+    await dbConnection.sync({force: true});
     console.log(`Example app listening on port ${port}`)
 })
