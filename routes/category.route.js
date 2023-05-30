1e167948742888dc7831e5d31437e25e1d0500d0
Mahmoud Haj Ali <mahmoudhajali2000@gmail.com>
2023-05-30 18:12:05 +0300

    add category route

--- routes/category.route.js
+++ routes/category.route.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const multer = require("multer");
 const { addCategory } = require('../controllers/category.controller');
 
-const uploadMutler = multer({dest: 'files/categories/'});
+const uploadMutler = multer({dest: __dirname + 'files/categories/'});
 
 router.post('/category', uploadMutler.any(),  addCategory);
 
