436148753c02d32d45b89ecad6c232b617b1a7c3
Mahmoud Haj Ali <mahmoudhajali2000@gmail.com>
2023-05-31 14:29:39 +0300

    add form

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,25 +1,46 @@
 const BaseResponse = require('../models/base_response');
 const Category = require('../models/category');
+const formidable = require('formidable');
 const { validateSuperAdmin } = require("../others/validator");
 
 const addCategory = async (req, res) => {
     try {
+
+        const data = await getFileFromReq(req);
+        console.log(data);
+        return res.send("test");
         const msg = await validateSuperAdmin(req.headers['authorization']);
         if(msg) return res.send(new BaseResponse({success: false, status: 403, msg: msg}));
         const { arName, enName } = req.body;
-        let image;
-        if(req.files){
-            image = req.files[0];
-            console.log(image);
-        }
-        await Category.create();
-        res.send(new BaseResponse({data: {},success: true, msg: "success"}));
+        const category = await Category.create({imageUrl: req.file.location, arName, enName});
+        res.send(new BaseResponse({data: category,success: true, msg: "success"}));
     } catch (error) {
         // console.log(error);
         res.send(new BaseResponse({success: false, msg: error.message}));
     }
 };
 
+
+function getFileFromReq(req){
+    return new Promise((resolve, reject) => {
+        const form = formidable({ multiples: true });
+        form.parse(req, (error, fields, files) => {
+            if(error){
+                reject(error);
+                return;
+            }
+            resolve({...fields, ...files});
+        });
+        // const chunks = [];
+        // req.on('data',(data)=>{
+        //     chunks.push(data);
+        // });
+        // req.on('error', reject);
+        // req.on('end', () => {
+        //     resolve(Buffer.concat(chunks).toString());
+        // });
+    });
+}
 
 
 module.exports = { addCategory};
--- routes/category.route.js
+++ routes/category.route.js
@@ -1,9 +1,26 @@
 const router = require('express').Router();
-const multer = require("multer");
 const { addCategory } = require('../controllers/category.controller');
+// const { S3Client } = require('@aws-sdk/client-s3');
+// const multer = require('multer');
+// const multerS3 = require('multer-s3');
 
-const uploadMutler = multer({dest: 'temp/files/categories/'});
+// const s3 = new S3Client()
 
-router.post('/category', uploadMutler.any(),  addCategory);
+// const upload = multer({
+//     storage: multerS3({
+//       s3: s3,
+//       bucket: process.env.CYCLIC_BUCKET_NAME,
+//       region: process.env.AWS_REGION,
+//       acl: "public-read",
+//       metadata: function (req, file, cb) {
+//         cb(null, {fieldName: file.fieldname});
+//       },
+//       key: function (req, file, cb) {
+//         cb(null, Date.now().toString())
+//       }
+//     })
+//   })
+
+router.post('/category', addCategory);
 
 module.exports = router;
