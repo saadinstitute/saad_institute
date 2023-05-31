cd14ac32ec9fbe9da385822ec86c52a500ef4441
Mahmoud Haj Ali <mahmoudhajali2000@gmail.com>
2023-05-31 15:53:45 +0300

    add cloudinary for saving images

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,35 +1,42 @@
 const BaseResponse = require('../models/base_response');
 const Category = require('../models/category');
+const cloudinary = require('cloudinary').v2;
 const formidable = require('formidable');
 const { validateSuperAdmin } = require("../others/validator");
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const addCategory = async (req, res) => {
     try {
 
         const data = await getFileFromReq(req);
-        console.log(data);
-        return res.send("test");
+        const resCloudinary = await cloudinary.uploader.upload(data.image.filepath);
+        console.log(resCloudinary);
         const msg = await validateSuperAdmin(req.headers['authorization']);
-        if(msg) return res.send(new BaseResponse({success: false, status: 403, msg: msg}));
-        const { arName, enName } = req.body;
-        const category = await Category.create({imageUrl: req.file.location, arName, enName});
-        res.send(new BaseResponse({data: category,success: true, msg: "success"}));
+        if (msg) return res.send(new BaseResponse({ success: false, status: 403, msg: msg }));
+        const { arName, enName } = data;
+        const category = await Category.create({ imageUrl: resCloudinary.url, arName, enName });
+        res.send(new BaseResponse({ data: category, success: true, msg: "success" }));
     } catch (error) {
         // console.log(error);
-        res.send(new BaseResponse({success: false, msg: error.message}));
+        res.send(new BaseResponse({ success: false, msg: error.message }));
     }
 };
 
 
-function getFileFromReq(req){
+function getFileFromReq(req) {
     return new Promise((resolve, reject) => {
         const form = formidable({ multiples: true });
         form.parse(req, (error, fields, files) => {
-            if(error){
+            if (error) {
                 reject(error);
                 return;
             }
-            resolve({...fields, ...files});
+            resolve({ ...fields, ...files });
         });
         // const chunks = [];
         // req.on('data',(data)=>{
@@ -43,4 +50,4 @@ function getFileFromReq(req){
 }
 
 
-module.exports = { addCategory};
+module.exports = { addCategory };
