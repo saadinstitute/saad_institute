1815b441a17c241bbe08a35f2e93bf58ee87efaa
Mahmoud Haj Ali <mahmoudhajali2000@gmail.com>
2023-06-04 04:42:34 +0300

    fix dashboard route

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const { getHome} = require('../controllers/dashboard.controller');
 
 
-router.get('/dashnoard', getHome);
+router.get('/dashboard', getHome);
 
 module.exports = router;
