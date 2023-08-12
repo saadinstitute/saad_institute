4d500a7d392368434a69a3227985f246ff7e401c
Mahmoud Haj Ali <mahmoudhajali2000@gmail.com>
2023-08-12 23:58:43 +0300

    add_popularity

---
+++ others/add_popularity.js
@@ -0,0 +1,24 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const dbConnection = require("../database/db");
+const Resturant = require('../models/resturant');
+//hellowat
+//hello
+const add_popularity = async (resturant_id, num_of_point  , user_id) => {
+
+ // const data = Resturant.findOne();
+const resturant = await Resturant.findByPk(resturant_id);
+
+  if (resturant.userId == user_id) {
+    console.log("no");
+  }else{
+  const m = resturant.popularity + num_of_point;    
+  console.log(resturant_id);
+  console.log(m);
+  console.log(resturant.arName);
+  console.log(resturant.enName);
+  console.log(resturant.popularity);
+  resturant.popularity=m;
+  resturant.save(); 
+}
+}
+module.exports = add_popularity;
