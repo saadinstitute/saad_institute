4faa9af1170b19702d9e083a897a7721dd25ce80
Mahmoud Haj Ali <mahmoudhajali2000@gmail.com>
2023-08-14 22:49:33 +0300

    fix notificaiton

--- others/send_notification.js
+++ others/send_notification.js
@@ -4,16 +4,18 @@ const admin = require('./firebase');
 const notification_options = {
     priority: "high",
     timeToLive: 60 * 60 * 24
-  };
-const sendNotification = async ({token, title, body, userId, resId, orderId}) => {
-    const n = await Notification.create({ reservationId: resId, userId, title, body, orderId: orderId});
+};
+const sendNotification = async ({ token, title, body, userId, resId, orderId }) => {
+    const n = await Notification.create({ reservationId: resId, userId, title, body, orderId: orderId });
     const message_notification = {
         notification: {
             title: title,
             body: body
         }
     };
-    await admin.messaging().sendToDevice(token, message_notification, notification_options);
+    if (token) {
+        await admin.messaging().sendToDevice(token, message_notification, notification_options);
+    }
 }
 
 
