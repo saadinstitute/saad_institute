72bd8dee6ddf5237ef9ddd161e2b44018bcf1a73
Mahmoud Haj Ali <mahmoudhajali2000@gmail.com>
2023-08-14 21:10:28 +0300

    modify add_popularity

--- others/add_popularity.js
+++ others/add_popularity.js
@@ -7,7 +7,7 @@ const add_popularity = async (resturant_id, num_of_point, user_id) => {
 
   // const data = Resturant.findOne();
   const resturant = await Resturant.findByPk(resturant_id);
-
+  console.log(resturant);
   if (resturant.userId == user_id) {
     console.log("no");
   } else {
