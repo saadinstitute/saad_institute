19910c61c17459803e96ad358d9714a7cba08d0f
Mahmoud Haj Ali <mahmoudhajali2000@gmail.com>
2023-08-15 10:23:18 +0300

    update add popularity

--- others/add_popularity.js
+++ others/add_popularity.js
@@ -6,19 +6,24 @@ const Resturant = require('../models/resturant');
 const add_popularity = async (resturant_id, num_of_point, user_id) => {
 
   // const data = Resturant.findOne();
-  const resturant = await Resturant.findByPk(resturant_id);
-  console.log(resturant);
-  if (resturant.userId == user_id) {
-    console.log("no");
-  } else {
-    const m = resturant.popularity + num_of_point;
-    console.log(resturant_id);
-    console.log(m);
-    console.log(resturant.arName);
-    console.log(resturant.enName);
-    console.log(resturant.popularity);
-    resturant.popularity = m;
-    resturant.save();
+  try {
+    const resturant = await Resturant.findByPk(resturant_id);
+    console.log(resturant);
+    if (resturant.userId === user_id) {
+      console.log("no");
+    } else {
+      const m = resturant.popularity + num_of_point;
+      console.log(resturant_id);
+      console.log(m);
+      console.log(resturant.arName);
+      console.log(resturant.enName);
+      console.log(resturant.popularity);
+      resturant.popularity = m;
+      resturant.save();
+    }
+  } catch (error) {
+console.log(error);
   }
+
 }
 module.exports = add_popularity;
