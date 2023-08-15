2262eb27c43eceaa17e6f8cadd640c0f3aea7521
Mahmoud Haj Ali <mahmoudhajali2000@gmail.com>
2023-08-15 11:31:33 +0300

    add more count

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -5,6 +5,9 @@ const User = require('../models/user');
 const Meal = require('../models/meal');
 const Charity = require('../models/meal');
 const { Op } = require("sequelize");
+const Delivery = require('../models/delivery');
+const Order = require('../models/order');
+const Course = require('../models/course');
 
 const getHome = async (req, res) => {
     const lang = req.headers["lang"];
@@ -14,7 +17,10 @@ const getHome = async (req, res) => {
         const categoriesCount = await Category.count();
         const mealsCount = await Meal.count();
         const charityCount = await Charity.count();
-        let data = {resturantsCount, usersCount, categoriesCount, charityCount, mealsCount, ordersCount: 0};
+        const ordersCount = await Order.count();
+        const deliveryCount = await Delivery.count();
+        const courseCount = await Course.count();
+        let data = {resturantsCount, usersCount, categoriesCount, charityCount, mealsCount, ordersCount, deliveryCount, courseCount};
         res.send(new BaseResponse({data, success: true, msg: "success", lang}));
     } catch (error) {
         res.send(new BaseResponse({success: false, msg: error, lang}));
