1221638c74d726f75d5257b6a4f12f1977ebc118
Mahmoud Haj Ali <mahmoudhajali2000@gmail.com>
2023-11-05 00:18:03 +0300

    fix attendance

--- routes/attendance.route.js
+++ routes/attendance.route.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const attencanceController = require('../controllers/attendance.controller');
 
-router.post('/attencance', attencanceController.addAttencance);
-router.get('/attencance/list', attencanceController.getStudentAttendance);
-router.put('/attencance', attencanceController.updateAttendance);
-router.delete('/attencance/:id', attencanceController.deleteAttendance);
+router.post('/attendance', attencanceController.addAttencance);
+router.get('/attendance/list', attencanceController.getStudentAttendance);
+router.put('/attendance', attencanceController.updateAttendance);
+router.delete('/attendance/:id', attencanceController.deleteAttendance);
 
 module.exports = router;
